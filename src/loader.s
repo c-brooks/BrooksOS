aa05d4c6588d7218679bbe0e0212068d413e2fe7
Corey <c.brooks96@live.ca>
2016-12-26 17:24:46 -0800

    Reserving memory space for kernel stack

--- src/loader.s
+++ src/loader.s
@@ -4,6 +4,12 @@ MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
 FLAGS        equ 0x0            ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER  ; calculate checksum
                                 ; (magic num + checksum + flags should = 0)
+KERNEL_STACK_SIZE equ 4096      ; size of stack (bytes)
+
+section .bss
+align 4
+kernel_stack:                   ; label points to beginning of memory
+    resb KERNEL_STACK_SIZE      ; reserve stack for kernel
 
 section .text:                  ; start of the text (code) section
 align 4                         ; the code must be 4 byte aligned
@@ -13,5 +19,8 @@ align 4                         ; the code must be 4 byte aligned
 
 loader:                         ; define loader label (entry point in linker script)
     mov eax, 0xCAFEBABE         ; place number 0xCAFEBABE in the register eax
+    mov esp, kernel_stack + KERNEL_STACK_SIZE
+                                ; point esp to start of stack
+                                
 .loop:
     jmp .loop                   ; loop
